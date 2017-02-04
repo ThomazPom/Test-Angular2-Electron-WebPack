55018b91d21f0b2b7ec7e6a88a63151fb98ec305
Thomas Pomme <Thomas.benhamou@hotmail.fr>
2017-02-04 12:39:45 +0100

    Building

---
+++ src/app/components/accueil/accueil.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Import decorators and services from angular
+ */
+import {Component, OnInit} from '@angular/core';
+import {   } from '@angular/material/'
+
+@Component({
+  selector: 'ae-acceuil',
+  templateUrl: './accueil.component.html',
+  styleUrls: ['./accueil.component.scss'],
+})
+export class AccueilComponent implements OnInit{
+  ngOnInit() {
+  }
+}
